ecc96a3e212eeeab1517ac1c13a22133cfb4e7d5
lbbboy <lbbboy@gmail.com>
2025-08-21 23:40:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,7 @@ sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds
 #sed -i '$a  src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
 # lua-maxminddb
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+# git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 
 # luci-app-vssr
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+# git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
