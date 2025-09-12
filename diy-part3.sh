c57952eb97a2525ff949297b342d5e01bf09349d
lbbboy <lbbboy@gmail.com>
2025-09-13 03:11:01 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -10,15 +10,10 @@
 
 #修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-material/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
-#修改immortalwrt.lan关联IP
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-#添加编译日期标识
-sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_MARK-$WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
-
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
 #修改默认IP地址
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
+sed -i "s/192\.168\.[0-9]\+\.[0-9]\+/192.168.123.1/g" "$CFG_FILE"
 
 # 隐藏顶部左侧的品牌文字
 cat >> package/feeds/luci/luci-theme-material/htdocs/luci-static/material/custom.css <<'EOF'
