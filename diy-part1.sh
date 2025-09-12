6e1c973e09aab10a42043ea06f5e61edfbbbe9e3
root <lbbboy@gmail.com>
2025-09-12 10:31:14 +0800

    Initial commit

---
+++ diy-part1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# fw876/helloworld
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaultault
+
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+
+#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+
+#sed -i '$a  src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+
+# lua-maxminddb
+# git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+
+# luci-app-vssr
+# git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
