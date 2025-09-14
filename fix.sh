e38a665d7e5bad5602688f6d1e44664ef6c0a121
lbbboy <lbbboy@gmail.com>
2025-09-15 07:38:08 +0800

    Create fix.sh

---
+++ fix.sh
@@ -0,0 +1,3 @@
+#替换nss_packages hash 
+sed -i 's/3ec87f221e8905d4b6b8b3d207b7f7c4666c3bc8db7c1f06d4ae2e78f863b8f4/881cbf75efafe380b5adc91bfb1f68add5e29c9274eb950bb1e815c7a3622807/' \
+  feeds/nss_packages/firmware/nss-firmware/Makefile
