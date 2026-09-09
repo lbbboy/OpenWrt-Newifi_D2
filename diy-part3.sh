d6cc556b2fbc920fb69924ebcdab590d74681c11
lbbboy <lbbboy@gmail.com>
2026-09-10 04:16:07 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -36,3 +36,30 @@ sed -i 's/GO_PKG_TAGS:=embedallowed,trace/GO_PKG_TAGS:=embedallowed/' \
 
 sed -i '/go generate trace\/trace.go/d' \
     feeds/packages/net/daed/Makefile
+
+# daed: 修复 32-bit MIPS 下 cilium/ebpf BTF DECL_TAG
+# ComponentIdx = 0xffffffff 在 32-bit Go int 下被错误判断为越界
+python3 - <<'PY'
+from pathlib import Path
+
+p = Path("feeds/packages/net/daed/Makefile")
+s = p.read_text()
+
+old = '''                go generate ./... ; \\
+                cd dae-core ; \\
+'''
+
+new = '''                go generate ./... ; \\
+                cd dae-core ; \\
+                EBPF_DIR="$$(go list -m -f '{{.Dir}}' github.com/cilium/ebpf)" ; \\
+                echo "==> cilium/ebpf: $$EBPF_DIR" ; \\
+                sed -i 's/if uint64(btfIndex) > math.MaxInt {/if btfIndex != ^uint32(0) \\\\&\\\\& uint64(btfIndex) > math.MaxInt {/' \\
+                        "$$EBPF_DIR/btf/unmarshal.go" ; \\
+'''
+
+if old not in s:
+    raise SystemExit("ERROR: daed Build/Compile patch target not found")
+
+p.write_text(s.replace(old, new, 1))
+print("==> daed: injected cilium/ebpf MIPS BTF patch")
+PY
